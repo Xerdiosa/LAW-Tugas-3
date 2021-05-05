a254dbc91dc15bd2be5b36b8c09388573e93c4be
Muhammad Aulia Akbar <aulia.aa.akbar@gmail.com>
2021-05-06 01:11:31 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Quickstart
+
+jalankan di 3 terminal berbeda
+
+``` bash
+docker-compose up
+go run publish_time.go
+go run chat.go
+```
+
+buka webserver di localhost:8000
---
+++ publish_time.go
@@ -0,0 +1,53 @@
+// modified from https://www.rabbitmq.com/tutorials/tutorial-four-go.html
+package main
+
+import (
+	"log"
+	"time"
+
+	"github.com/streadway/amqp"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
+func main() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	err = ch.ExchangeDeclare(
+		"log",    // name
+		"fanout", // type
+		true,     // durable
+		false,    // auto-deleted
+		false,    // internal
+		false,    // no-wait
+		nil,      // arguments
+	)
+	failOnError(err, "Failed to declare an exchange")
+
+	for {
+		dt := time.Now()
+		body := dt.Format("2006-01-02 15:04")
+		err = ch.Publish(
+			"log", // exchange
+			"",    // routing key
+			false, // mandatory
+			false, // immediate
+			amqp.Publishing{
+				ContentType: "text/plain",
+				Body:        []byte(body),
+			})
+		failOnError(err, "Failed to publish a message")
+		log.Printf(" [x] Sent %s", body)
+		time.Sleep(60 * time.Second)
+	}
+}
